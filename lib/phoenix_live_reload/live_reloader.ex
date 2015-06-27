4b0fb517122be21dedea0705f690d342672c30a8
DainisL <dainis186@gmail.com>
2015-06-27 21:11:24 +0300

    Added warning console message if html response has a missing body tag

--- lib/phoenix_live_reload/live_reloader.ex
+++ lib/phoenix_live_reload/live_reloader.ex
@@ -1,6 +1,5 @@
 defmodule Phoenix.LiveReloader do
   use Phoenix.Router
-
   @moduledoc """
   Router for live-reload detection in development.
 
@@ -70,26 +69,52 @@ defmodule Phoenix.LiveReloader do
   defp before_send_inject_reloader(conn) do
     register_before_send conn, fn conn ->
       resp_body = to_string(conn.resp_body)
-
-      if inject?(conn, resp_body) do
-        [page | rest] = String.split(resp_body, "</body>")
-        body = page <> reload_assets_tag() <> Enum.join(["</body>" | rest], "")
-
-        put_in conn.resp_body, body
+      if inject?(conn) do
+        put_in conn.resp_body, build_response_body(resp_body)
       else
         conn
       end
     end
   end
 
-  defp inject?(conn, resp_body) do
+  defp build_response_body(resp_body) do
+    if body_tag_present?(resp_body) do
+      add_iframe_tag(resp_body)
+    else
+      add_warnig_message(resp_body)
+    end
+  end
+
+  defp body_tag_present?(resp_body) do
+    String.contains?(resp_body, "<body")
+    |> Kernel.&&(String.contains?(resp_body, "</body>"))
+  end
+
+  defp add_warnig_message(resp_body) do
+    (resp_body <> missing_tag)
+  end
+
+  defp add_iframe_tag(resp_body) do
+    [page | rest] = String.split(resp_body, "</body>")
+    page <> reload_assets_tag() <> Enum.join(["</body>" | rest], "")
+  end
+
+  defp inject?(conn) do
     conn
     |> get_resp_header("content-type")
     |> html_content_type?
-    |> Kernel.&&(String.contains?(resp_body, "<body"))
+    |> Kernel.&&(is_not_ajax?(conn))
   end
+
+  defp is_not_ajax?(conn), do: (get_req_header(conn, "x-requested-with") != ["XMLHttpRequest"])
   defp html_content_type?([]), do: false
   defp html_content_type?([type | _]), do: String.starts_with?(type, "text/html")
+
+  defp missing_tag() do
+    """
+    <script> console.warn('Phoenix live reload is enabled but the <body> tag missing'); </script>
+    """
+  end
 
   defp reload_assets_tag() do
     """
