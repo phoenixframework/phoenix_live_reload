0b153b379db740e1315c62bb0c896b961d518595
Chris McCord <chris@chrismccord.com>
2015-08-12 09:23:31 -0400

    Do not check origin

--- lib/phoenix_live_reload/socket.ex
+++ lib/phoenix_live_reload/socket.ex
@@ -6,8 +6,13 @@ defmodule Phoenix.LiveReloader.Socket do
   use Phoenix.Socket
   channel "phoenix:live_reload", Phoenix.LiveReload.Channel
 
-  transport :websocket, Phoenix.Transports.WebSocket, transport_log: false
-  transport :longpoll, Phoenix.Transports.LongPoll, transport_log: false
+  transport :websocket, Phoenix.Transports.WebSocket,
+    transport_log: false,
+    check_origin: false
+
+  transport :longpoll, Phoenix.Transports.LongPoll,
+    transport_log: false,
+    check_origin: false
 
   def connect(_params, socket), do: {:ok, socket}
 
