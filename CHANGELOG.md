a024e1d9c5f7350f8d0d1efbc9b4c386b240cdad
Chris McCord <chris@chrismccord.com>
2015-09-18 10:21:13 -0400

    Release 1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 1.0.1 (2015-9-18)
+
+* Bug fixes
+  * Fix issue causing stylesheet link taps to duplicate on reload
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixLiveReload.Mixfile do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.0.1"
 
   def project do
     [app: :phoenix_live_reload,
