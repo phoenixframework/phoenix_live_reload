279656ce14060677310585f669204b07a04ec561
Chris McCord <chris@chrismccord.com>
2016-01-07 13:32:18 -0500

    Release 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.0.2 (2015-1-7)
+
+* Bug fixes
+  * Fix issue where iframe path did not respect script_name
+
 ## 1.0.1 (2015-9-18)
 
 * Bug fixes
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixLiveReload.Mixfile do
   use Mix.Project
 
-  @version "1.0.1"
+  @version "1.0.2"
 
   def project do
     [app: :phoenix_live_reload,
