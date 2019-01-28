93453b215c04ce26eb9e3f57994f2e9110ead562
Lewis Chung <lewis@shopwith.co>
2019-01-28 00:55:08 -0600

    Revert useless formatting changes

--- priv/static/phoenix_live_reload.js
+++ priv/static/phoenix_live_reload.js
@@ -1,4 +1,4 @@
-var buildFreshUrl = function(link) {
+var buildFreshUrl = function(link){
   var date = Math.round(Date.now() / 1000).toString();
   var url = link.href.replace(/(\&|\\?)vsn=\d*/, '');
   var newLink = document.createElement('link');
@@ -9,46 +9,37 @@ var buildFreshUrl = function(link) {
   };
 
   newLink.onerror = onComplete;
-  newLink.onload = onComplete;
+  newLink.onload  = onComplete;
   link.setAttribute('data-pending-removal', '');
   newLink.setAttribute('rel', 'stylesheet');
   newLink.setAttribute('type', 'text/css');
-  newLink.setAttribute(
-    'href',
-    url + (url.indexOf('?') >= 0 ? '&' : '?') + 'vsn=' + date
-  );
+  newLink.setAttribute('href', url + (url.indexOf('?') >= 0 ? '&' : '?') +'vsn=' + date);
   link.parentNode.insertBefore(newLink, link.nextSibling);
 
   return newLink;
 };
 
-var repaint = function() {
+var repaint = function(){
   var browser = navigator.userAgent.toLowerCase();
-  if (browser.indexOf('chrome') > -1) {
-    setTimeout(function() {
-      document.body.offsetHeight;
-    }, 25);
+  if(browser.indexOf('chrome') > -1){
+    setTimeout(function(){ document.body.offsetHeight; }, 25);
   }
 };
 
-var cssStrategy = function() {
+var cssStrategy = function(){
   var reloadableLinkElements = window.parent.document.querySelectorAll(
     'link[rel=stylesheet]:not([data-no-reload]):not([data-pending-removal])'
   );
 
   [].slice
     .call(reloadableLinkElements)
-    .filter(function(link) {
-      return link.href;
-    })
-    .forEach(function(link) {
-      buildFreshUrl(link);
-    });
+    .filter(function(link) { return link.href })
+    .forEach(function(link) { buildFreshUrl(link) });
 
   repaint();
 };
 
-var pageStrategy = function(chan) {
+var pageStrategy = function(chan){
   chan.off('assets_change');
   window.parent.location.reload();
 };
@@ -59,12 +50,9 @@ var reloadStrategies = {
 };
 
 socket.connect();
-var chan = socket.channel('phoenix:live_reload', {});
+var chan = socket.channel('phoenix:live_reload', {})
 chan.on('assets_change', function(msg) {
-  var reloadStrategy =
-    reloadStrategies[msg.asset_type] || reloadStrategies.page;
-  setTimeout(function() {
-    reloadStrategy(chan);
-  }, interval);
+  var reloadStrategy = reloadStrategies[msg.asset_type] || reloadStrategies.page;
+  setTimeout(function(){ reloadStrategy(chan); }, interval);
 });
 chan.join();
