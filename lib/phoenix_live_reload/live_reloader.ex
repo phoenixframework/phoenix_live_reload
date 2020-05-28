928eab8a0a4242f51d691dcff20e58a85bfc32f5
Łukasz Jan Niemier <lukasz@niemier.pl>
2020-05-28 12:07:33 +0200

    Use iolist for adding refresh iframe (#102)

--- lib/phoenix_live_reload/live_reloader.ex
+++ lib/phoenix_live_reload/live_reloader.ex
@@ -119,17 +119,16 @@ defmodule Phoenix.LiveReloader do
 
   defp before_send_inject_reloader(conn, endpoint) do
     register_before_send(conn, fn conn ->
-      if conn.resp_body != nil && html?(conn) do
-        resp_body = IO.iodata_to_binary(conn.resp_body)
-        if has_body?(resp_body) and :code.is_loaded(endpoint) do
-          [page | rest] = String.split(resp_body, "</body>")
-          body = page <> reload_assets_tag(conn, endpoint) <> Enum.join(["</body>" | rest], "")
-          put_in conn.resp_body, body
-        else
-          conn
-        end
+      with %{resp_body: resp_body} when not is_nil(resp_body) <- conn,
+           true <- html?(conn),
+           true <- has_body?(resp_body),
+           {:file, _} <- :code.is_loaded(endpoint),
+           [leading, trailing] <- :binary.split(resp_body, "</body>")
+      do
+        tag = reload_assets_tag(conn, endpoint)
+        put_in conn.resp_body, [leading, tag, "</body>", trailing]
       else
-        conn
+        _ -> conn
       end
     end)
   end
@@ -145,9 +144,8 @@ defmodule Phoenix.LiveReloader do
 
   defp reload_assets_tag(conn, endpoint) do
     path = conn.private.phoenix_endpoint.path("/phoenix/live_reload/frame#{suffix(endpoint)}")
-    """
-    <iframe src="#{path}" style="display: none;"></iframe>
-    """
+
+    ["<iframe src=\"", path, "\" style=\"display: none;\"></iframe>\n"]
   end
 
   defp suffix(endpoint), do: endpoint.config(:live_reload)[:suffix] || ""
