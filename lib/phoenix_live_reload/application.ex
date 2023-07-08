682aca3b01cf70b9eefdda2791965cfad063f4f7
crbelaus <cristian@crbelaus.com>
2023-07-08 17:53:11 +0200

    Replace deprecated Logger.warn call

--- lib/phoenix_live_reload/application.ex
+++ lib/phoenix_live_reload/application.ex
@@ -29,7 +29,7 @@ defmodule Phoenix.LiveReloader.Application do
         {:ok, pid}
 
       other ->
-        Logger.warn("""
+        Logger.warning("""
         Could not start Phoenix live-reload because we cannot listen to the file system.
         You don't need to worry! This is an optional feature used during development to
         refresh your browser when you save files and it does not affect production.
